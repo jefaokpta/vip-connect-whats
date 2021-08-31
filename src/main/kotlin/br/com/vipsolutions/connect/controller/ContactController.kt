008130fa9a2e78c4d7dd4794e0a5a29806817b36
jefaokpta <jefaokpta@hotmail.com>
2021-08-31 08:49:33 -0300

    melhor apresentacao da descricao de agentes online

--- src/main/kotlin/br/com/vipsolutions/connect/controller/ContactController.kt
+++ src/main/kotlin/br/com/vipsolutions/connect/controller/ContactController.kt
@@ -1,9 +1,12 @@
 package br.com.vipsolutions.connect.controller
 
 import br.com.vipsolutions.connect.model.Contact
+import br.com.vipsolutions.connect.model.ws.AgentSession
+import br.com.vipsolutions.connect.model.ws.AgentSessionDAO
 import br.com.vipsolutions.connect.repository.ContactRepository
 import br.com.vipsolutions.connect.util.ContactCenter
 import br.com.vipsolutions.connect.websocket.SessionCentral
+import kotlinx.coroutines.reactive.collect
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.server.ResponseStatusException
@@ -24,7 +27,7 @@ class ContactController(private val contactRepository: ContactRepository) {
 
     @GetMapping("/memory/agent/{company}")
     fun listMemoryAgentSessions(@PathVariable company: Long) = Mono.justOrEmpty(SessionCentral.agents[company])
-        .map { it.keys.toList() }
+        .map { it.values.map(::AgentSessionDAO) }
         .switchIfEmpty( Mono.error(ResponseStatusException(HttpStatus.NOT_FOUND, "lista não encontrado.")))
 
 //    @GetMapping
---
+++ src/main/kotlin/br/com/vipsolutions/connect/model/ws/AgentSessionDAO.kt
@@ -0,0 +1,20 @@
+package br.com.vipsolutions.connect.model.ws
+
+import br.com.vipsolutions.connect.model.Contact
+import org.springframework.web.reactive.socket.WebSocketSession
+
+/**
+ * @author Jefferson Alves Reis (jefaokpta) < jefaokpta@hotmail.com >
+ * Date: 2021-06-22
+ */
+class AgentSessionDAO(
+    val session: String,
+    var contact: Contact?,
+    val category: MutableList<Int> = mutableListOf()
+) {
+    constructor(agentSession: AgentSession): this (
+        agentSession.session.id,
+        agentSession.contact,
+        agentSession.category
+    )
+}
