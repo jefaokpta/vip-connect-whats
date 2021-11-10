b942ada81737ea4eda7ff3c14a63799231ed6446
jefaokpta <jefaokpta@hotmail.com>
2021-11-10 18:38:59 -0300

    correcao - precisava ser flatmap pra executar o finalize

--- src/main/kotlin/br/com/vipsolutions/connect/service/WsChatHandlerService.kt
+++ src/main/kotlin/br/com/vipsolutions/connect/service/WsChatHandlerService.kt
@@ -37,7 +37,7 @@ class WsChatHandlerService(
                     .map { sendQuizButtonsMessage(contact, it) }
             }else{
                 resetContact(contact)
-                    .map (::finalizeAttendance)
+                    .flatMap (::finalizeAttendance)
             }
         }
 
